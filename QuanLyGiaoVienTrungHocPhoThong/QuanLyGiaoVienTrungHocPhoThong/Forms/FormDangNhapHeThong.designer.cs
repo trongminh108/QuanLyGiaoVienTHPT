9ba4dc242faed8f65911941fe44d9d4d94fef1dd
Minh Trong <lmtrong.ctc2020@gmail.com>
2022-11-02 07:20:14 +0700

    add custom button

--- QuanLyGiaoVienTrungHocPhoThong/QuanLyGiaoVienTrungHocPhoThong/Forms/FormDangNhapHeThong.designer.cs
+++ QuanLyGiaoVienTrungHocPhoThong/QuanLyGiaoVienTrungHocPhoThong/Forms/FormDangNhapHeThong.designer.cs
@@ -37,8 +37,8 @@
             this.txtNhap = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.radGhiNho = new System.Windows.Forms.RadioButton();
-            this.btnDangNhap = new System.Windows.Forms.Button();
             this.picAGU = new System.Windows.Forms.PictureBox();
+            this.roundedButton1 = new QuanLyGiaoVienTrungHocPhoThong.Customdesign.RoundedButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAGU)).BeginInit();
             this.SuspendLayout();
@@ -90,7 +90,7 @@
             // txtMK
             // 
             this.txtMK.Location = new System.Drawing.Point(226, 169);
-            this.txtMK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(2);
             this.txtMK.Multiline = true;
             this.txtMK.Name = "txtMK";
             this.txtMK.Size = new System.Drawing.Size(273, 28);
@@ -101,7 +101,7 @@
             this.txtNhap.BackColor = System.Drawing.SystemColors.Window;
             this.txtNhap.CausesValidation = false;
             this.txtNhap.Location = new System.Drawing.Point(226, 119);
-            this.txtNhap.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNhap.Margin = new System.Windows.Forms.Padding(2);
             this.txtNhap.Multiline = true;
             this.txtNhap.Name = "txtNhap";
             this.txtNhap.Size = new System.Drawing.Size(273, 28);
@@ -110,7 +110,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(76, 98);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(456, 149);
             this.pictureBox1.TabIndex = 12;
@@ -121,7 +121,7 @@
             this.radGhiNho.AutoSize = true;
             this.radGhiNho.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radGhiNho.Location = new System.Drawing.Point(226, 211);
-            this.radGhiNho.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radGhiNho.Margin = new System.Windows.Forms.Padding(2);
             this.radGhiNho.Name = "radGhiNho";
             this.radGhiNho.Size = new System.Drawing.Size(88, 25);
             this.radGhiNho.TabIndex = 13;
@@ -129,40 +129,43 @@
             this.radGhiNho.Text = "Ghi nhớ";
             this.radGhiNho.UseVisualStyleBackColor = true;
             // 
-            // btnDangNhap
-            // 
-            this.btnDangNhap.BackgroundImage = global::QuanLyGiaoVienTrungHocPhoThong.Properties.Resources.simplebutton;
-            this.btnDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnDangNhap.FlatAppearance.BorderSize = 0;
-            this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDangNhap.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDangNhap.Location = new System.Drawing.Point(240, 266);
-            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(141, 71);
-            this.btnDangNhap.TabIndex = 14;
-            this.btnDangNhap.Text = "Đăng nhập";
-            this.btnDangNhap.UseVisualStyleBackColor = true;
-            // 
             // picAGU
             // 
             this.picAGU.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.picAGU.Image = ((System.Drawing.Image)(resources.GetObject("picAGU.Image")));
             this.picAGU.Location = new System.Drawing.Point(9, 10);
-            this.picAGU.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picAGU.Margin = new System.Windows.Forms.Padding(2);
             this.picAGU.Name = "picAGU";
             this.picAGU.Size = new System.Drawing.Size(78, 78);
             this.picAGU.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picAGU.TabIndex = 15;
             this.picAGU.TabStop = false;
             // 
-            // Dangnhaphethong
+            // roundedButton1
+            // 
+            this.roundedButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.roundedButton1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.roundedButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.roundedButton1.BorderRadius = 10;
+            this.roundedButton1.BorderSize = 0;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.ForeColor = System.Drawing.Color.White;
+            this.roundedButton1.Location = new System.Drawing.Point(215, 289);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(150, 40);
+            this.roundedButton1.TabIndex = 16;
+            this.roundedButton1.Text = "roundedButton1";
+            this.roundedButton1.TextColor = System.Drawing.Color.White;
+            this.roundedButton1.UseVisualStyleBackColor = false;
+            // 
+            // FormDangNhapHeThong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.roundedButton1);
             this.Controls.Add(this.picAGU);
-            this.Controls.Add(this.btnDangNhap);
             this.Controls.Add(this.radGhiNho);
             this.Controls.Add(this.txtMK);
             this.Controls.Add(this.txtNhap);
@@ -171,8 +174,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Dangnhaphethong";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormDangNhapHeThong";
             this.Text = "Đăng Nhập Hệ Thống";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAGU)).EndInit();
@@ -191,8 +194,8 @@
         private System.Windows.Forms.TextBox txtNhap;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RadioButton radGhiNho;
-        private System.Windows.Forms.Button btnDangNhap;
         private System.Windows.Forms.PictureBox picAGU;
+        private Customdesign.RoundedButton roundedButton1;
     }
 }
 
