67c2cdf5222044cbdddda694c9d36b098c4fddc0
skinsportlight2018 <skinsportlight2018@gmail.com>
2022-11-02 09:00:46 +0700

    thiet ke giao dien

--- QuanLyGiaoVienTrungHocPhoThong/QuanLyGiaoVienTrungHocPhoThong/Forms/FormQuenMatKhau.designer.cs
+++ QuanLyGiaoVienTrungHocPhoThong/QuanLyGiaoVienTrungHocPhoThong/Forms/FormQuenMatKhau.designer.cs
@@ -29,45 +29,32 @@
         private void InitializeComponent()
         {
             this.btnLui = new System.Windows.Forms.Button();
-            this.btnMaXN = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtTenDangNhap = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnGuiMaXN = new QuanLyGiaoVienTrungHocPhoThong.Customdesign.RoundedButton();
             this.SuspendLayout();
             // 
             // btnLui
             // 
             this.btnLui.BackColor = System.Drawing.Color.LightSkyBlue;
             this.btnLui.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLui.Location = new System.Drawing.Point(14, 19);
-            this.btnLui.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLui.Location = new System.Drawing.Point(19, 23);
+            this.btnLui.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLui.Name = "btnLui";
-            this.btnLui.Size = new System.Drawing.Size(62, 39);
+            this.btnLui.Size = new System.Drawing.Size(83, 48);
             this.btnLui.TabIndex = 20;
             this.btnLui.Text = "Lùi";
             this.btnLui.UseVisualStyleBackColor = false;
-            // 
-            // btnMaXN
-            // 
-            this.btnMaXN.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnMaXN.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMaXN.Location = new System.Drawing.Point(220, 232);
-            this.btnMaXN.Margin = new System.Windows.Forms.Padding(2);
-            this.btnMaXN.Name = "btnMaXN";
-            this.btnMaXN.Size = new System.Drawing.Size(194, 39);
-            this.btnMaXN.TabIndex = 16;
-            this.btnMaXN.Text = "Gửi mã xác nhận";
-            this.btnMaXN.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(86, 153);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(115, 188);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 21);
+            this.label3.Size = new System.Drawing.Size(135, 26);
             this.label3.TabIndex = 14;
             this.label3.Text = "Địa chỉ email";
             // 
@@ -75,10 +62,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 106);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(115, 130);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 21);
+            this.label1.Size = new System.Drawing.Size(151, 26);
             this.label1.TabIndex = 13;
             this.label1.Text = "Tên đăng nhập";
             // 
@@ -86,37 +72,56 @@
             // 
             this.txtTenDangNhap.BackColor = System.Drawing.SystemColors.Window;
             this.txtTenDangNhap.CausesValidation = false;
-            this.txtTenDangNhap.Location = new System.Drawing.Point(220, 106);
-            this.txtTenDangNhap.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenDangNhap.Location = new System.Drawing.Point(293, 130);
+            this.txtTenDangNhap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTenDangNhap.Multiline = true;
             this.txtTenDangNhap.Name = "txtTenDangNhap";
-            this.txtTenDangNhap.Size = new System.Drawing.Size(243, 28);
+            this.txtTenDangNhap.Size = new System.Drawing.Size(323, 34);
             this.txtTenDangNhap.TabIndex = 21;
             // 
             // txtEmail
             // 
             this.txtEmail.BackColor = System.Drawing.SystemColors.Window;
             this.txtEmail.CausesValidation = false;
-            this.txtEmail.Location = new System.Drawing.Point(220, 147);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Location = new System.Drawing.Point(293, 181);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtEmail.Multiline = true;
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(243, 28);
+            this.txtEmail.Size = new System.Drawing.Size(323, 34);
             this.txtEmail.TabIndex = 22;
             // 
-            // QuenMK
+            // btnGuiMaXN
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnGuiMaXN.BackColor = System.Drawing.Color.Blue;
+            this.btnGuiMaXN.BackgroundColor = System.Drawing.Color.Blue;
+            this.btnGuiMaXN.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnGuiMaXN.BorderRadius = 20;
+            this.btnGuiMaXN.BorderSize = 0;
+            this.btnGuiMaXN.FlatAppearance.BorderSize = 0;
+            this.btnGuiMaXN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuiMaXN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuiMaXN.ForeColor = System.Drawing.Color.MistyRose;
+            this.btnGuiMaXN.Location = new System.Drawing.Point(293, 292);
+            this.btnGuiMaXN.Name = "btnGuiMaXN";
+            this.btnGuiMaXN.Size = new System.Drawing.Size(202, 52);
+            this.btnGuiMaXN.TabIndex = 23;
+            this.btnGuiMaXN.Text = "Gửi mã xác nhận";
+            this.btnGuiMaXN.TextColor = System.Drawing.Color.MistyRose;
+            this.btnGuiMaXN.UseVisualStyleBackColor = false;
+            // 
+            // FormQuenMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuiMaXN);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtTenDangNhap);
             this.Controls.Add(this.btnLui);
-            this.Controls.Add(this.btnMaXN);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "QuenMK";
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormQuenMatKhau";
             this.Text = "Quên Mật Khẩu";
             this.Load += new System.EventHandler(this.QuenMK_Load);
             this.ResumeLayout(false);
@@ -127,10 +132,10 @@
         #endregion
 
         private System.Windows.Forms.Button btnLui;
-        private System.Windows.Forms.Button btnMaXN;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtTenDangNhap;
         private System.Windows.Forms.TextBox txtEmail;
+        private QuanLyGiaoVienTrungHocPhoThong.Customdesign.RoundedButton btnGuiMaXN;
     }
 }
